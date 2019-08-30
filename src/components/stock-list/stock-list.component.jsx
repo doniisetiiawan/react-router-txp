7e4b93dc0a4b92f018abcfd8c4e0b001ab8296a5
Doni Setiawan <donisetiawan.050@gmail.com>
2019-08-30 21:09:25 +0700

    Add prompt

--- src/components/stock-list/stock-list.component.jsx
+++ src/components/stock-list/stock-list.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Prompt, Route } from 'react-router-dom';
 
 const StatsComponent = () => (
   <div> Showing Stats </div>
@@ -29,6 +29,13 @@ const STOCK_ROUTES = [
 ];
 
 class StockListComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFormSubmitted: false,
+    };
+  }
+
   componentDidMount() {
     console.log('Inside StockList component\'s componentWillMount fn');
   }
@@ -37,8 +44,18 @@ class StockListComponent extends Component {
     console.log('Inside User component\'s componentWillUnmount fn');
   }
 
+  handleFormSubmit = () => {
+    // event.prevenDefault();
+    this.setState({ isFormSubmitted: true });
+  };
+
+  handleInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
   render() {
     const { match } = this.props;
+    const { isFormSubmitted } = this.state;
 
     return (
       <div>
@@ -52,6 +69,32 @@ class StockListComponent extends Component {
               />
             ))
         }
+        <div className="form-group">
+          <label>Username</label>
+          <input id="username" type="text" onChange={this.handleInputChange} />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input id="password" type="password" onChange={this.handleInputChange} />
+        </div>
+        <div className="form-group">
+          <label>Confirm Password</label>
+          <input id="confirm-password" type="password" onChange={this.handleInputChange} />
+        </div>
+        <button onClick={this.handleFormSubmit} type="submit">
+          Submit
+        </button>
+        {/* <Prompt message="Are you sure?" /> */}
+        {/* <Prompt */}
+        {/*  when={isFormSubmitted} */}
+        {/*  message="Are you sure?" */}
+        {/* /> */}
+        <Prompt
+          when={isFormSubmitted}
+          message={
+            (location) => `Are you sure you want to ${location.pathname}?`
+          }
+        />
       </div>
     );
   }
