d66244ef4621b05ea55b6dd6d5603613b79664e5
Doni Setiawan <donisetiawan.050@gmail.com>
2019-08-30 19:39:05 +0700

    Navigating to a route

--- src/components/dashboard/dashboard.component.jsx
+++ src/components/dashboard/dashboard.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const DashboardComponent = () => (
+const DashboardComponent = (props) => (
   <div className="dashboard">
     Inside Dashboard route
+    <button type="button" onClick={() => props.history.push('/user')}>
+      User
+    </button>
   </div>
 );
 
