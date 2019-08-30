2947c9c6ba5bf2d828db4d0d3b386807653a61ea
Doni Setiawan <donisetiawan.050@gmail.com>
2019-08-30 17:32:06 +0700

    InnerRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 import HomeComponent from './components/home/home.component';
 import DashboardComponent from './components/dashboard/dashboard.component';
@@ -7,85 +7,105 @@ import CategoryComponent from './components/category/category.component';
 import StockListComponent from './components/stock-list/stock-list.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/dashboard">Dashboard</Link>
-      </nav>
-      <Route
-        path="/"
-        component={HomeComponent}
-        exact
-      />
-      <Route
-        path="/dashboard/"
-        component={DashboardComponent}
-        strict
-      />
-      <Route
-        path="/Dashboard"
-        component={DashboardComponent}
-        sensitive
-      />
-      <Route
-        path="/dashboard"
-        component={DashboardComponent}
-        sensitive
-      />
-      <Route
-        path="/user"
-        render={() => (
-          <div> Inside User Route </div>
-        )}
-      />
-      <Route
-        path="/sidenav"
-        children={() => (
-          <div> Inside Sidenav route </div>
-        )}
-      />
-      <Route
-        path="/user"
-        render={({ match }) => {
-          console.log(match);
-          return (
-            <div> Inside User route </div>
-          );
-        }}
-      />
-      <Route
-        path="/sidenav"
-        children={({ match }) => {
-          console.log(match);
-          return (
+class App extends Component {
+  refCallback = (node) => {
+    node.onmouseover = () => {
+      node.focus();
+    };
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link
+            to="/"
+            innerRef={this.refCallback}
+          >
+            Home
+          </Link>
+          <Link
+            to="/dashboard"
+            innerRef={this.refCallback}
+          >
+            Dashboard
+          </Link>
+        </nav>
+        <Route
+          path="/"
+          component={HomeComponent}
+          exact
+        />
+        <Route
+          path="/dashboard/"
+          component={DashboardComponent}
+          strict
+        />
+        <Route
+          path="/Dashboard"
+          component={DashboardComponent}
+          sensitive
+        />
+        <Route
+          path="/dashboard"
+          component={DashboardComponent}
+          sensitive
+        />
+        <Route
+          path="/user"
+          render={() => (
+            <div> Inside User Route </div>
+          )}
+        />
+        <Route
+          path="/sidenav"
+          children={() => (
             <div> Inside Sidenav route </div>
-          );
-        }}
-      />
-      <Route
-        path="/github/:githubID/:twitterID?"
-        component={GitHubComponent}
-      />
-      <Route
-        path="/github/:githubID(\w+)"
-        component={GitHubComponent}
-      />
-      <Route
-        path="/user/:userID(\d+)"
-        component={GitHubComponent}
-      />
-      <Route
-        path="/category"
-        component={CategoryComponent}
-      />
-      <Route
-        path="/stocks"
-        component={StockListComponent}
-      />
-    </div>
-  );
+          )}
+        />
+        <Route
+          path="/user"
+          render={({ match }) => {
+            console.log(match);
+            return (
+              <div> Inside User route </div>
+            );
+          }}
+        />
+        <Route
+          path="/sidenav"
+          children={({ match }) => {
+            console.log(match);
+            return (
+              <div> Inside Sidenav route </div>
+            );
+          }}
+        />
+        <Route
+          path="/github/:githubID/:twitterID?"
+          component={GitHubComponent}
+        />
+        <Route
+          path="/github/:githubID(\w+)"
+          component={GitHubComponent}
+        />
+        <Route
+          path="/user/:userID(\d+)"
+          component={GitHubComponent}
+        />
+        <Route
+          path="/category"
+          component={CategoryComponent}
+        />
+        <Route
+          path="/stocks"
+          component={StockListComponent}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
