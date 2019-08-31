f25633d6483d5e662eafe0b32e0abe31c60477ba
Doni Setiawan <donisetiawan.050@gmail.com>
2019-08-31 08:23:47 +0700

    Redirect component

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
-const HomeComponent = () => (
-  <div>
-    Inside Home route
-  </div>
-);
+function HomeComponent() {
+  return (
+    <Redirect to="/dashboard" />
+  );
+}
 
 export default HomeComponent;
