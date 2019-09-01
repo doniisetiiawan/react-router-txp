ada5c41aac5b7a97e29c2876a93d314f1ca67b8c
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-01 12:32:38 +0700

    Force refresh prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  // refCallback = (node) => {
-  //   node.onmouseover = () => {
-  //     node.focus();
-  //   };
-  // };
+const App = () => (
+  <BrowserRouter basename="/admin" forceRefresh>
+    <div className="component">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </nav>
 
-  render() {
-    return (
-      <BrowserRouter basename="/admin">
-        <div className="component">
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/dashboard">Dashboard</Link>
-          </nav>
-
-          <Route
-            path="/"
-            render={() => <div> Inside Home </div>}
-            exact
-          />
-          <Route
-            path="/dashboard"
-            render={({ location }) => <div> In Dashboard, Location Key: {location.key} </div>}
-          />
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+      <Route
+        path="/"
+        render={() => <div> Inside Home </div>}
+        exact
+      />
+      <Route
+        path="/dashboard"
+        render={({ location }) => <div> In Dashboard, Location Key: {location.key} </div>}
+      />
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
