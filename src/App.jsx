82a53fa907ac5ec7f710fe39b6a99ad3f2a91090
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-01 13:09:59 +0700

    getUserConfirmation prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Link, Route, Prompt,
+} from 'react-router-dom';
 import './App.css';
 
-const App = () => (
-  <BrowserRouter keyLength={10}>
-    <div className="container">
-      <nav>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/user">User</Link>
-      </nav>
-      <Route
-        path="/dashboard"
-        render={({ location }) => <div> In Dashboard, Location Key: {location.key} </div>}
-      />
-      <Route
-        path="/user"
-        render={({ location }) => <div> In User, Location Key: {location.key} </div>}
-      />
-    </div>
-  </BrowserRouter>
-);
+class App extends React.Component {
+  userConfirmationFunc = (message, callback) => {
+    const status = window.confirm(message);
+    callback(status);
+  }
+
+  render() {
+    return (
+      <BrowserRouter getUserConfirmation={this.userConfirmationFunc}>
+        <div className="container">
+          <nav>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/user">User</Link>
+          </nav>
+          <Route
+            path="/dashboard"
+            render={({ location }) => <div> In Dashboard, Location Key: {location.key} </div>}
+          />
+          <Route
+            path="/user"
+            render={({ location }) => (
+              <div> In User, Location Key: {location.key}
+                <Prompt message="This is shown in a confirmation window" />
+              </div>
+            )}
+          />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
