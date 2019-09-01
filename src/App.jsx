cd82e12f6455e8f7524c67449bb4b3277e4f5139
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-01 12:35:37 +0700

    Keylength prop

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,19 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => (
-  <BrowserRouter basename="/admin" forceRefresh>
-    <div className="component">
+  <BrowserRouter keyLength={10}>
+    <div className="container">
       <nav>
-        <Link to="/">Home</Link>
         <Link to="/dashboard">Dashboard</Link>
+        <Link to="/user">User</Link>
       </nav>
-
-      <Route
-        path="/"
-        render={() => <div> Inside Home </div>}
-        exact
-      />
       <Route
         path="/dashboard"
         render={({ location }) => <div> In Dashboard, Location Key: {location.key} </div>}
+      />
+      <Route
+        path="/user"
+        render={({ location }) => <div> In User, Location Key: {location.key} </div>}
       />
     </div>
   </BrowserRouter>
