fbccedf72c2abfc86dc1c95350c1c012c90fddb8
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-03 10:35:25 +0700

    Redux in react

---
+++ src/components/counter.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions/counter';
+
+const Counter = ({ count, increment, decrement }) => (
+  <div>
+    <h4>Counter</h4>
+    <button type="button" onClick={decrement}>-</button>
+    <span>{count}</span>
+    <button type="button" onClick={increment}>+</button>
+  </div>
+);
+
+const mapStateToProps = (state) => ({
+  count: state.count,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Counter);
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 // import { Router } from 'react-router';
 // import { createBrowserHistory } from 'history';
+
+import rootReducer from './reducers';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Counter from './components/counter.component';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(rootReducer);
 
 // const customHistory = createBrowserHistory();
 
@@ -14,7 +21,12 @@ import * as serviceWorker from './serviceWorker';
 //   </Router>,
 //   document.getElementById('root'),
 // );
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Counter />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
